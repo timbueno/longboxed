4408d14eef728cf8bd879173505bbc182e78adc4
Timothy Bueno <timbueno@gmail.com>
2013-08-22 21:29:44 -0400

    first try at adding celery to supervisor

--- longboxed/settings.py
+++ longboxed/settings.py
@@ -17,7 +17,6 @@ SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/longboxed'
 
 # Celery
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-# CELERY_BROKER_URL = 'amqp://celery_user:password@precise32:5672//'
 # CELERYBEAT_SCHEDULE = {
 #     'test-every-30-seconds': {
 #         'task': 'tasks.test',
