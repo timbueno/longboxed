194459cd4041091b124c25782a72df0051f9b84d
Timothy Bueno <timbueno@gmail.com>
2013-09-02 18:01:59 -0400

    improved comic book processing for new imports

--- longboxed/comics/__init__.py
+++ longboxed/comics/__init__.py
@@ -9,7 +9,8 @@ import csv
 import gzip
 import re
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from decimal import Decimal
 from HTMLParser import HTMLParser
 from itertools import groupby
 
@@ -58,29 +59,28 @@ class ComicService(object):
         self.titles = TitleService()
         self.issues = IssueService()
 
-    def insert_comic(self, p=None, t=None, i=None):
-        publisher = self.publishers.first(name=p['name'])
-        if not publisher:
-            # print 'Adding Publisher: %s' % p['name']
-            publisher = self.publishers.create(**p)
+    # def insert_comic(self, p=None, t=None, i=None):
+    #     publisher = self.publishers.first(name=p['name'])
+    #     if not publisher:
+    #         # print 'Adding Publisher: %s' % p['name']
+    #         publisher = self.publishers.create(**p)
 
-        title = self.titles.first(name=t['name'])
-        if not title:
-            # print 'Adding Title: %s' % t['name']
-            t['publisher'] = publisher
-            title = self.titles.create(**t)
+    #     title = self.titles.first(name=t['name'])
+    #     if not title:
+    #         # print 'Adding Title: %s' % t['name']
+    #         t['publisher'] = publisher
+    #         title = self.titles.create(**t)
 
-        i['publisher'] = publisher
-        i['title'] = title
-        issue = self.issues.first(diamond_id=i['diamond_id'])
-        if issue:
-            # print 'Updating: %s' % issue.complete_title
-            issue = self.issues.update(issue, **i)
-        else:
-            # print 'Adding Issue: %s' % i['complete_title']
-            issue = self.issues.create(**i)
-            
-        return
+    #     i['publisher'] = publisher
+    #     i['title'] = title
+    #     issue = self.issues.first(diamond_id=i['diamond_id'])
+    #     if issue:
+    #         # print 'Updating: %s' % issue.complete_title
+    #         issue = self.issues.update(issue, **i)
+    #     else:
+    #         # print 'Adding Issue: %s' % i['complete_title']
+    #         issue = self.issues.create(**i) 
+    #     return
 
     def insert_publisher(self, raw_publisher=None):
         publisher = self.publishers.first(name=raw_publisher)
@@ -120,7 +120,9 @@ class ComicService(object):
                 if item[-5] == 'Comics':
                     item = [element for element in item]
                     if item[19] in SUPPORTED_PUBS and self.is_diamond_id(item[20]):
-                        comics.append(item)
+                        release_date = datetime.strptime(item[12], '%Y-%m-%d')
+                        if release_date.date() > datetime.now().date() and release_date.date() < (datetime.now().date() + timedelta(days=21)):
+                            comics.append(item)
         print '...Done'
         return comics
 
@@ -195,11 +197,6 @@ class ComicService(object):
         issue_number_groups = []
         for k, g in title_groups:
             group = list(g)
-            # print k, list(group)
-            # print '\n--------------------'
-            # print k.name
-            # for issue in list(group):
-            #     print 'Info: %s | %s' % (issue.issue_number, issue.other)
             group = sorted(group, key=lambda x: x.issue_number)
             issue_number_group = groupby(group, key=lambda x: x.issue_number)
             issue_number_groups.append(issue_number_group)
@@ -211,55 +208,76 @@ class ComicService(object):
             # m = re.match(r'(?P<title>[^#]*[^#\s])\s*(?:#(?P<issue_number>(\d+))\s*)?(?:\(of (?P<issues>(\d+))\)\s*)?(?P<other>(.+)?)', title).groupdict()
             m = re.match(r'(?P<title>[^#]*[^#\s])\s*(?:#(?P<issue_number>([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?))\s*)?(?:\(of (?P<issues>(\d+))\)\s*)?(?P<other>(.+)?)', title).groupdict()
             m['complete_title'] = title
+            if m['issue_number']:
+                # print 'ORIGINAL ISSUE NUMBER: ', m['issue_number']
+                m['issue_number'] = Decimal(m['issue_number'])
+                # print 'CONVERTED ISSUE NUMBER: ', m['issue_number']
+            if m['issues']:
+                m['issues'] = Decimal(m['issues'])
             # print '\n--------------'
             # print 'Full:    ', title
             # print 'Name:    ', m.get('title')
             # print 'Issue #: ', m.get('issue_number')
             # print 'Issues:  ', m.get('issues')
             # print 'Other:   ', m.get('other')
-
         except (AttributeError, TypeError):
             m = None
-
         return m
 
 
     def test_grouping(self):
+        flag = False
+        # Get raw text data from daily download
         raw_issues = self.get_raw_issues('dd.gz')
+        # Insert raw comic book into the database
         issue_list = []
         for q, raw_issue in enumerate(raw_issues):
-            # if raw_issue[19] in SUPPORTED_PUBS and self.is_diamond_id(raw_issue[20]):
             (i, t, p) = self.extract_issue_information(raw_issue)
             publisher = self.insert_publisher(p)
             title = self.insert_title(t, publisher)
             issue = self.insert_issue(i, title, publisher)
             issue_list.append(issue)
-            # print publisher, title, issue
             if q % 250 == 0:
                 print 'Saved %d / %d comics' % (q, len(raw_issues))
-            # if q == 1000:
-            #     break
+        # Group new issues based on title and issue_number
         groups = self.group_issues(issue_list)
+        # Process grouped issues
         for group in groups:
             for k, g in group:
-                issues = list(g)
+                # Look for pre-exisiting issue in the database but not in new download
+                new_issues = list(g)
+                w = self.issues.__model__.query.filter_by(title=new_issues[0].title, issue_number=k)
+                for i in w:
+                    if i not in new_issues:
+                        new_issues.append(i)
+                        flag = True
+                # Sort group based on Diamond ID and set a parent for display purposes
                 matches = []
-                for issue in issues:
+                for issue in new_issues:
                     match = re.search(r'\d+', issue.diamond_id)
                     matches.append((int(match.group()), match.string))
-                    # print 'Info: %s | %s | %s' % (issue.issue_number, issue.diamond_id, issue.other)
                 matches.sort(key=lambda x: x[0])
-                for issue in issues:
+                for issue in new_issues:
                     if issue.diamond_id == matches[0][1]:
                         issue.is_parent = True
-                        issue.alternates = [i for i in issues if i.diamond_id != issue.diamond_id]
-                        self.issues.save(issue)
-                for issue in issues:
-                    if issue.is_parent:
-                        print '\n--------------------'
-                        print 'Title: %s' % issues[0].title.name
-                        print 'Issue Number: %s' % k
-                        print 'Alternates: %d' % len(issue.alternates)
+                    else:
+                        issue.is_parent = False
+                    self.issues.save(issue)
+        # for group in groups:
+        #     for k, g in group:
+        #         issues = list(g)
+
+        #         matches = []
+        #         for issue in issues:
+        #             match = re.search(r'\d+', issue.diamond_id)
+        #             matches.append((int(match.group()), match.string))
+        #             # print 'Info: %s | %s | %s' % (issue.issue_number, issue.diamond_id, issue.other)
+        #         matches.sort(key=lambda x: x[0])
+        #         for issue in issues:
+        #             if issue.diamond_id == matches[0][1]:
+        #                 issue.is_parent = True
+        print 'DONE!'
+        print flag
 
 
 
