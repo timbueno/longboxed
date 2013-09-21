fb3617f34dd63f2469ad92d5fa125d39f93f366f
Timothy Bueno <timbueno@gmail.com>
2013-09-21 11:49:43 -0400

    added admin views for all models

--- longboxed/frontend/admin.py
+++ longboxed/frontend/admin.py
@@ -14,21 +14,39 @@ from ..models import Issue, Publisher, Title, User
 
 class IssueAdmin(ModelView):
     # List of columns that can be sorted
-    # column_sortable_list = ('issue_number', 'complete_title', ('publisher', Publisher.name))
-    column_list = ('issue_number', 'issues', 'complete_title', 'title', 'publisher')
-
-    form_ajax_refs = {
-        'publisher': ('name',)
-    }
-
+    column_sortable_list = ('issue_number', 'complete_title', 'on_sale_date', ('title',Title.name), ('publisher', Publisher.name))
+    # column_searchable_list = ('title')
+    column_list = ('on_sale_date', 'issue_number', 'issues', 'complete_title', 'title', 'publisher')
     def __init__(self, session):
         # Just call parent class with predefined model.
         super(IssueAdmin, self).__init__(Issue, session)
 
+
+class PublisherAdmin(ModelView):
+    def __init__(self, session):
+        # Just call parent class with predefined model.
+        super(PublisherAdmin, self).__init__(Publisher, session) 
+
+
+class TitleAdmin(ModelView):
+    column_sortable_list= ('name', ('publisher', Publisher.name))
+    # column_searchable_list = ('name')
+    def __init__(self, session):
+        # Just call parent class with predefined model.
+        super(TitleAdmin, self).__init__(Title, session)        
+
+
+class UserAdmin(ModelView):
+    column_list = ('email', 'last_login_at', 'login_count', 'pull_list')
+
+    def __init__(self, session):
+        # Just call parent class with predefined model.
+        super(UserAdmin, self).__init__(User, session)
+
+
 def init_app(app):
     admin = Admin(app)
-    admin.add_view(ModelView(User, db.session))
-    # admin.add_view(ModelView(Issue, db.session))
+    admin.add_view(UserAdmin(db.session))
     admin.add_view(IssueAdmin(db.session))
-    admin.add_view(ModelView(Publisher, db.session))
-    admin.add_view(ModelView(Title, db.session))
+    admin.add_view(PublisherAdmin(db.session))
+    admin.add_view(TitleAdmin(db.session))
