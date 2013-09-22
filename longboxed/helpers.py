3b72a1be392bc164e5965933a5855be35dc3f80a
Timothy Bueno <timbueno@gmail.com>
2013-09-22 00:38:24 -0400

    deleted old code on helpers

--- longboxed/helpers.py
+++ longboxed/helpers.py
@@ -70,10 +70,6 @@ def wednesday(date, multiplier=0):
 def current_wednesday():
     return wednesday(datetime.today().date())
 
-# This is going to cause problems when trying to import data on a sunday.
-# def get_current_wednesday():
-#     start, end = get_current_week()
-#     return get_wednesday(start)
 
 def get_week(date, multiplier=0):
     """Returns Sunday and Saturday of the week the 'date' argument is currently in.
@@ -84,19 +80,6 @@ def get_week(date, multiplier=0):
     saturday = (sunday + timedelta(days=6))
     return (sunday, saturday)
 
-# def get_current_week():
-#     today = datetime.today()
-#     day_of_week = today.weekday()
-#     to_beginning_of_week = timedelta(days=day_of_week)
-#     beginning_of_week = (today - to_beginning_of_week).replace(hour=0, minute=0, second=0, microsecond=0)
-#     # to_end_of_week = timedelta(days= (6 - day_of_week))
-#     # end_of_week = (today + to_end_of_week).replace(hour=0, minute=0, second=0, microsecond=0)
-#     end_of_week = beginning_of_week + timedelta(days=6)
-#     return (beginning_of_week.date(), end_of_week.date())
-
-# def get_next_week():
-#     start, end = get_current_week()
-#     return (start+timedelta(days=7), end+timedelta(days=7))
 
 class JsonSerializer(object):
     """A mixin that can be used to mark a SQLAlchemy model class which
