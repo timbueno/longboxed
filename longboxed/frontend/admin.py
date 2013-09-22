9f37b4d99daf31e7f0beb75c2b9d1a1e71e4b5e1
Timothy Bueno <timbueno@gmail.com>
2013-09-22 16:58:07 -0400

    added actions to set date of issues to issue admin view

--- longboxed/frontend/admin.py
+++ longboxed/frontend/admin.py
@@ -5,11 +5,16 @@
 
     frontend application asset "pipeline"
 """
+from flask import flash
 from flask.ext.security import current_user
 from flask.ext.admin import Admin, AdminIndexView
+from flask.ext.admin.actions import action
+from flask.ext.admin.babel import gettext, lazy_gettext
 from flask.ext.admin.contrib.sqlamodel import ModelView
 
 from ..core import db
+from ..helpers import current_wednesday, next_wednesday 
+from ..services import comics as _comics
 from ..models import Issue, Publisher, Title, User, Role
 
 
@@ -31,11 +36,31 @@ class IssueAdmin(AdministratorBase):
     # List of columns that can be sorted
     column_sortable_list = ('issue_number', 'complete_title', 'on_sale_date', ('title',Title.name), ('publisher', Publisher.name))
     # column_searchable_list = ('title')
-    column_list = ('on_sale_date', 'issue_number', 'issues', 'complete_title', 'title', 'publisher')
+    column_list = ('on_sale_date', 'diamond_id', 'issue_number', 'issues', 'complete_title', 'title', 'publisher')
+    
     def __init__(self, session):
         # Just call parent class with predefined model.
         super(IssueAdmin, self).__init__(Issue, session)
 
+    def set_on_sale_date(self, ids, date):
+        try:
+            issues = _comics.issues.get_all(*ids)
+            for issue in issues:
+                _comics.issues.update(issue, **{'on_sale_date': date})
+        except Exception, ex:
+            flash(gettext('Failed to set date %(error)s', error=str(ex)), 'error')
+
+    @action('current_wednesday', lazy_gettext('This Wed | %(date)s', date=current_wednesday()), lazy_gettext('Are you sure? | %(date)s', date=current_wednesday()))
+    def action_current_wednesday(self, ids):
+        self.set_on_sale_date(ids, current_wednesday())
+
+    @action('next_wednesday', lazy_gettext('Next Wed | %(date)s', date=next_wednesday()), lazy_gettext('Are you sure? | %(date)s', date=next_wednesday()))
+    def action_next_wednesday(self, ids):
+        self.set_on_sale_date(ids, next_wednesday())
+
+    @action('no_date', lazy_gettext('No Date'), lazy_gettext('Are you sure? | No Date'))
+    def action_no_date(self, ids):
+        self.set_on_sale_date(ids, None)
 
 class PublisherAdmin(AdministratorBase):
     def __init__(self, session):
