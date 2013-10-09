e0f4c3af7374cec41219b6c93b4b314b28bbe086
Timothy Bueno <timbueno@gmail.com>
2013-10-09 18:59:46 -0400

    added filter method to core service module

--- longboxed/core.py
+++ longboxed/core.py
@@ -127,7 +127,7 @@ class Service(object):
 
         :param *ids: instance ids
         """
-        return self.__model__query.filter(*criterion).all()
+        return self.__model__.query.filter(*criterion).all()
 
     def find(self, **kwargs):
         """Returns a list of instances of the service's model filtered by the
