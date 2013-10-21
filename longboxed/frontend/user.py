a6e3c2413058f0a96c4463d2dfef884d654597f5
Timothy Bueno <timbueno@gmail.com>
2013-10-20 21:18:55 -0400

    got twitter working.

--- longboxed/frontend/user.py
+++ longboxed/frontend/user.py
@@ -13,8 +13,12 @@ bp = Blueprint('users', __name__)
 
 @route(bp, '/profile', methods=['GET', 'POST', 'DELETE'])
 def profile():
+    twitter = current_app.social.twitter
+    # twitter.get_api()
+    # print twitter.get_connection()
     return render_template(
         'profile.html',
+        twitter_conn=twitter.get_connection(),
         facebook_conn=current_app.social.facebook.get_connection(),
         google_conn=current_app.social.google.get_connection()
     )
