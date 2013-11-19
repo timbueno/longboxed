c2f12ff4cfa6373e046a837200c90869d4f731fc
Timothy Bueno <timbueno@gmail.com>
2013-11-19 18:49:15 -0500

    crude pagination and bundles loaded set to 5

--- longboxed/frontend/user.py
+++ longboxed/frontend/user.py
@@ -12,6 +12,7 @@ from . import route
 # from ..services import comics as _comics
 # from ..services import bundle as _bundles
 from ..helpers import current_wednesday, refresh_bundle
+from ..models import Bundle
 
 bp = Blueprint('users', __name__)
 
@@ -28,5 +29,8 @@ def social():
 @route(bp, '/profile')
 def profile():
     # bundle = list(_comics.issues.__model__.query.filter().limit(20))
-    bundle = refresh_bundle(current_user, current_wednesday())
-    return render_template('profile.html', bundles=current_user.bundles)
+    # bundle = refresh_bundle(current_user, current_wednesday())
+    # bundles = current_user.bundles.order_by(Bundle.release_date.desc()).limit(5)
+    bundles = current_user.bundles.order_by(Bundle.release_date.desc()).paginate(page=1, per_page=5)
+    print bundles.pages
+    return render_template('profile.html', bundles=bundles.items)
