7f9fba62690019042a74db1b3a167577d4376f86
Timothy Bueno <timbueno@gmail.com>
2014-02-12 17:27:32 -0500

    cleaning up

--- longboxed/frontend/dashboard.py
+++ longboxed/frontend/dashboard.py
@@ -5,15 +5,12 @@
 
     Frontend blueprints
 """
-from flask import Blueprint, g, redirect, render_template, request, url_for
-from flask.ext.security import (current_user, login_required)
-from flask.ext.wtf import Form
-from wtforms import BooleanField, SelectField, SelectMultipleField
+from flask import Blueprint, g, render_template
+from flask.ext.security import current_user
 
 from . import route
 from ..helpers import current_wednesday
 from ..services import comics as _comics
-from ..services import users as _users
 
 
 bp = Blueprint('dashboard', __name__)
@@ -35,60 +32,3 @@ def test():
 def index():
     issues = _comics.issues.__model__.query.filter(_comics.issues.__model__.on_sale_date == current_wednesday(), _comics.issues.__model__.is_parent == True).order_by(_comics.issues.__model__.popularity).limit(4)
     return render_template('index.html', issues=issues)
-
-
-@route(bp, '/settings', methods=['GET','POST'])
-@login_required
-def settings():
-    if current_user is not None:
-        if request.method == 'POST':
-            if 'display_favs' in request.values:
-                current_user.display_pull_list = True
-            else:
-                current_user.display_pull_list = False
-            current_user.default_cal = request.form['cals']
-
-            pubs = [long(e) for e in request.form.getlist('publishers')]
-            p = _comics.publishers.get_all(*pubs)
-            current_user.publishers = p
-            _users.save(current_user)
-    else:
-        return redirect(url_for('index'))
-
-    # Default calendars
-    calendars = _users.get_calendar_info(current_user) # Get a users calendar
-    c = []
-    for cal in calendars[1]:
-        c.append((cal[0], cal[1]))
-    # Set the default calendar
-    default_cal = current_user.default_cal
-
-    # Get all publishers
-    pubs = [(p.id, p.name) for p in _comics.publishers.all() if p.name != ''] #########
-    pubs.sort()
-    # Get user defaults
-    user_pubs = [p.id for p in current_user.publishers if p.name !='']
-
-
-
-    class ExampleForm(Form):
-        display_favs = BooleanField(
-                            'Display Favorites',
-                            description=u'If a comic on your favorites list matches an issue out that week, display it inline with the other books.', 
-                            default = current_user.display_pull_list)
-        cals = SelectField(
-                    u'Calendars',
-                    description=u'Set the calendar the you want to add comics to.',
-                    choices=c,
-                    default=default_cal)
-        publishers = SelectMultipleField(
-            u'Publishers',
-            {'title':'Select Publishers'},
-            description=u'Publishers to display (selecting none displays all)',
-            choices=pubs,
-            default=user_pubs
-        )
-
-    form = ExampleForm()
-
-    return render_template('settings.html',form=form)
