f671b2f47279d5362c6ab72453917ae2999fad34
Timothy Bueno <timbueno@gmail.com>
2014-02-15 16:14:06 -0500

    renamed index.html to splash.html in preparation to have a dashboard view replace the splash.html view for logged in users

--- longboxed/frontend/dashboard.py
+++ longboxed/frontend/dashboard.py
@@ -31,4 +31,4 @@ def test():
 @route(bp, '/')
 def index():
     issues = _comics.issues.__model__.query.filter(_comics.issues.__model__.on_sale_date == current_wednesday(), _comics.issues.__model__.is_parent == True).order_by(_comics.issues.__model__.popularity).limit(4)
-    return render_template('index.html', issues=issues)
+    return render_template('splash.html', issues=issues)
