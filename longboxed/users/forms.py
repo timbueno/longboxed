4789acf456c998dd239b508258dac527853a1f4f
Timothy Bueno <timbueno@gmail.com>
2014-02-16 17:27:19 -0500

    added settings view and ability to delete account

--- longboxed/users/forms.py
+++ longboxed/users/forms.py
@@ -5,3 +5,14 @@
 
     User forms
 """
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import DataRequired, InputRequired
+
+class UserInformationForm(Form):
+    first_name = TextField('First Name', validators=[InputRequired()])
+    last_name = TextField('Last Name', validators=[InputRequired()])
+
+class DeleteUserAccountForm(Form):
+    delete_confirmation = BooleanField('I understand.', validators=[DataRequired()])
+
