538f9dd69b862d0798b307ea6506c41e2dcd9b57
Timothy Bueno <timbueno@gmail.com>
2014-06-30 23:01:45 -0400

    added method to add a title to a users pull list

--- longboxed/api_v1/pull_list.py
+++ longboxed/api_v1/pull_list.py
@@ -10,6 +10,7 @@ from flask import Blueprint, g, jsonify, request
 
 from . import route
 from .authentication import auth
+from .errors import bad_request
 from ..services import users, comics
 
 
@@ -28,13 +29,17 @@ def get_pull_list():
 @route(bp, '/', methods=['POST'])
 @auth.login_required
 def add_title_to_pull_list():
-    print 'IN THE HANDLER'
-    title_id = request.args.get('id', type=int)
-    print title_id
-    print request.args
-    title = comics.titles.get_or_404(title_id)
-    g.current_user.append(title)
-    users.save(g.current_user)
+    title_id = request.json.get('title_id')
+    if title_id:
+        title_id = int(title_id)
+        title = comics.titles.get_or_404(title_id)
+    else:
+        return bad_request('title_id: attribute not found')
+    if title not in g.current_user.pull_list:
+        g.current_user.pull_list.append(title)
+        g.current_user = users.save(g.current_user)
+    else:
+        return bad_request('Title is already on users pull list')
     return jsonify({
             'user': g.current_user.email,
             'pull_list': [title.to_json() for title in g.current_user.pull_list]
