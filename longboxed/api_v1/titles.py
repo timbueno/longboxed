06c2883a97c85b0a196234d6c734c6ae46d29532
Timothy Bueno <timbueno@gmail.com>
2014-07-01 22:42:36 -0400

    added autocomplete endpoint and changed titles to get_titles

--- longboxed/api_v1/titles.py
+++ longboxed/api_v1/titles.py
@@ -6,7 +6,8 @@
     Title endpoints
 """
 
-from flask import Blueprint, jsonify, request, url_for
+from flask import abort, Blueprint, jsonify, request, url_for
+from sqlalchemy.orm.exc import NoResultFound
 
 from ..services import comics
 from . import route
@@ -16,16 +17,16 @@ bp = Blueprint('titles', __name__, url_prefix='/titles')
 
 
 @route(bp, '/')
-def titles():
+def get_titles():
     page = request.args.get('page', 1, type=int)
     pagination = comics.titles.__model__.query.paginate(page, per_page=20, error_out=False)
     titles = pagination.items
     prev = None
     if pagination.has_prev:
-        prev = url_for('.titles', page=page-1, _external=True)
+        prev = url_for('.get_titles', page=page-1, _external=True)
     next = None
     if pagination.has_next:
-        next = url_for('.titles', page=page+1, _external=True)
+        next = url_for('.get_titles', page=page+1, _external=True)
     return jsonify({
         'titles': [title.to_json() for title in titles],
         'prev': prev,
@@ -47,3 +48,24 @@ def get_issues_for_title(id):
         'title': title.name,
         'issues': [issue.to_json() for issue in title.issues]
     })
+
+
+@route(bp, '/autocomplete/', methods=['GET'])
+def autocomplete():
+    if 'query' not in request.args.keys():
+        abort(404)
+    fragment = request.args.get('query')
+    keywords = fragment.split()
+    searchstring = '%%'.join(keywords)
+    searchstring = '%%%s%%' % (searchstring)
+    try:
+        Titles = comics.titles.__model__
+        res = Titles.query.filter(Titles.name.ilike(searchstring)).limit(20)
+        return jsonify({
+                'query': fragment,
+                'suggestions': [r.name for r in res],
+                'data': ['%s' % (r.name) for r in res]
+            })
+    except NoResultFound:
+        return jsonify({'query': fragment, 'suggestions':[], 'data':[]})
+    return jsonify({'data': 'YOU DID IT!'})
