7c367f4e5d7d6d9aec603153e640a4a87920347a
Timothy Bueno <timbueno@gmail.com>
2014-07-01 22:41:32 -0400

    about to delete these old endpoints (bundles and pull_list)

--- longboxed/api_v1/bundles.py
+++ longboxed/api_v1/bundles.py
@@ -6,27 +6,27 @@
     Bundle endpoints
 """
 
-from flask import Blueprint, g, jsonify
+# from flask import Blueprint, g, jsonify
 
-from . import route
-from .authentication import auth
-from ..services import bundle as _bundles
-
-
-bp = Blueprint('bundles', __name__, url_prefix='/bundles')
+# from . import route
+# from .authentication import auth
+# from ..services import bundle as _bundles
 
 
-@route(bp, '/')
-@auth.login_required
-def get_all_bundles():
-    bundles = g.current_user.bundles
-    return jsonify({
-        'bundles': [bundle.to_json() for bundle in bundles]
-    })
+# bp = Blueprint('bundles', __name__, url_prefix='/bundles')
 
 
-@route(bp, '/<int:id>')
-@auth.login_required
-def get_bundle(id):
-    bundle = _bundles.get(id)
-    return jsonify(bundle.to_json())
+# @route(bp, '/')
+# @auth.login_required
+# def get_all_bundles():
+#     bundles = g.current_user.bundles
+#     return jsonify({
+#         'bundles': [bundle.to_json() for bundle in bundles]
+#     })
+
+
+# @route(bp, '/<int:id>')
+# @auth.login_required
+# def get_bundle(id):
+#     bundle = _bundles.get(id)
+#     return jsonify(bundle.to_json())
