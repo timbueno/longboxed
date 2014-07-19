8968b631b6608dc2203ef9ee06a3431bf1462c5c
Timothy Bueno <timbueno@gmail.com>
2014-07-19 16:54:45 -0400

    removed comics.forms dependency on services

--- longboxed/comics/forms.py
+++ longboxed/comics/forms.py
@@ -9,11 +9,14 @@ from flask.ext.wtf import Form
 from wtforms import TextField
 from wtforms.validators import InputRequired, ValidationError
 
-from ..services import comics as _comics
+# from ..services import comics as _comics
+from ..models import Title
 
 
 def validate_title(form, field):
-    if not _comics.titles.first(name=field.data):
+    # if not _comics.titles.first(name=field.data):
+    #     raise ValidationError('Given title is not in Longboxed')
+    if not Title.query.filter_by(name=field.data).first():
         raise ValidationError('Given title is not in Longboxed')
 
 class AddToPullList(Form):
