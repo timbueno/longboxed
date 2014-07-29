0e62b765721f62ab5fb4a1cc00b275907c05ad93
Timothy Bueno <timbueno@gmail.com>
2014-07-28 21:25:27 -0400

    removed services from __init__ files

--- longboxed/users/__init__.py
+++ longboxed/users/__init__.py
@@ -5,37 +5,3 @@
 
     longboxed users package
 """
-import requests
-
-from ..core import Service
-from .models import Role, User
-
-class UsersService(Service):
-    __model__ = User
-
-    def get_calendar_info(self, user):
-        headers = {'Authorization': 'Bearer '+user.access_token}
-        data = {'minAccessRole':'owner'}
-        endpoint = 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
-        response = requests.get(endpoint, headers=headers, params=data)
-        r = response.json()
-
-        calendars = []
-        default_cal = None
-        for cal in r['items']:
-            name = cal['summary']
-            calid = cal['id']
-            try:
-                if cal['primary']:
-                    default_cal = (cal['id'], cal['summary'], True)
-                    primary = True
-            except KeyError:
-                primary = False
-
-            calendars.append((calid, name, primary))
-
-        return (default_cal, calendars)
-
-
-class RolesService(Service):
-    __model__ = Role
