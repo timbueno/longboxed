8b6f909e492813077d69f277bfa5cf6c726cb7c3
Timothy Bueno <timbueno@gmail.com>
2014-09-07 14:59:30 -0400

    Title.get_latest_released_issue() method now only includes `parent` titles

--- longboxed/comics/models.py
+++ longboxed/comics/models.py
@@ -319,6 +319,7 @@ class Title(db.Model, CRUDMixin):
         else:
             date = current_wednesday()
         i = self.issues.filter(
+                            Issue.is_parent == True,
                             Issue.on_sale_date <= date,
                             Issue.on_sale_date != None)\
                        .order_by(Issue.on_sale_date.desc())\
