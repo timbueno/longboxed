7dbc5fb5f9f82f1df76fb3aaff0600fbf95cf1be
Tim Bueno <timbueno@gmail.com>
2014-12-11 19:26:42 -0500

    added DiamondList model and a bunch of methods

---
+++ longboxed/downloads/models.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+    longboxed.downloads.models
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Download models
+"""
+import requests
+
+from datetime import datetime
+from hashlib import md5
+from StringIO import StringIO
+
+from bs4 import BeautifulSoup
+from flask import current_app
+
+from ..core import db, CRUDMixin
+from ..helpers import unicode_csv_reader, week_handler
+from ..models import Issue
+
+
+diamonds_issues = db.Table('diamonds_issues',
+        db.Column('issue_id', db.Integer, db.ForeignKey('issues.id')),
+        db.Column('diamonds_id', db.Integer, db.ForeignKey('diamond_lists.id'))
+)
+
+
+class DiamondList(db.Model, CRUDMixin):
+    __tablename__ = 'diamond_lists'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    date_created = db.Column(db.DateTime(), default=datetime.utcnow)
+    date = db.Column(db.Date())
+    hash_string = db.Column(db.String(100), unique=True)
+    issue_link_time = db.Column(db.DateTime())
+    source = db.Column(db.Text())
+    revision = db.Column(db.Integer())
+
+    issues = db.relationship(
+            'Issue',
+            secondary=diamonds_issues,
+            backref=db.backref('diamond_lists', lazy='dynamic'),
+            lazy='dynamic'
+    )
+
+    def __init__(self):
+        pass
+
+    def download(self, week, affiliate_id=None):
+        """
+        Gets file containing a list of shippments from Diamond Distributers.
+        This file is served from TFAW's servers. Three files are available at
+        any given time; thisweek, nextweek, twoweeks. They describe shipments
+        pertaining to their respective timeframes.
+
+        :param week: String designating which diamond list to download
+                     Options: 'thisweek', 'nextweek', 'twoweeks'
+        """
+        date = week_handler(week)
+        base_url = 'http://www.tfaw.com/intranet/diamondlists_raw.php'
+        payload = {
+            'mode': week,
+            'uid': affiliate_id,
+            'show%5B%5D': 'Comics',
+            'display': 'text_raw'
+        }
+        r = requests.get(base_url, params=payload)
+
+        html = BeautifulSoup(r.content)
+        self.source = html.pre.string.strip(' \t\n\r')
+        self.date = date
+        return (self.source, self.date)
+
+    def hash_source(self):
+        if self.source == None:
+            raise Exception('Source is not set!')
+        self.hash_string = md5(self.source).hexdigest()
+        return self.hash_string
+
+    def clean_diamond_id(self, diamond_id):
+        if diamond_id[-1:].isalpha():
+            diamond_id = diamond_id[:-1]
+        return diamond_id
+
+    def link_issues(self, fieldnames, supported_publishers):
+        f = StringIO(self.source)
+        reader = unicode_csv_reader(f, fieldnames)
+        data = [row for row in reader]
+        issues = []
+        for row in data:
+            if Issue.check_release_relevancy(row, supported_publishers):
+                diamond_id = self.clean_diamond_id(row['diamond_id'])
+                issue = Issue.query.filter_by(diamond_id=diamond_id).first()
+                if issue:
+                    issues.append(issue)
+        if issues:
+            self.issues = issues
+        else:
+            self.issues = []
+        return self.issues
+
+    @classmethod
+    def download_and_process(cls, week, fieldnames, supported_publishers):
+        d_list = None
+        new_list = cls.new()
+        source, date = new_list.download(week)
+        hash_string = new_list.hash_source()
+        old_list = cls.query.filter_by(hash_string=hash_string).first()
+        if not old_list:
+            latest_list = cls.query.filter(
+                                       cls.date==date)\
+                                   .order_by(cls.revision.desc())\
+                                   .first()
+            if latest_list:
+                print 'Earlier lists found!'
+                print 'This new revision: %s' % (latest_list.revision + 1)
+                new_list.revision = latest_list.revision + 1
+            else:
+                print 'No earlier revisions'
+                new_list.revision = 1
+            issues = new_list.link_issues(fieldnames, supported_publishers)
+            d_list = new_list.save()
+        return d_list
+
